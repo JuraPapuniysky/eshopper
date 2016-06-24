612e357ed3840e98a3fc427609e8a5c253de3bd3
wsst17 <jurapapunitsky@gmail.com>
2016-06-24 12:58:40 +0300

    theme add in process

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,8 +13,25 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/animate.css',
+        'css/bootstrap.min.css',
+        'css/font-awesome.min.css',
+        'css/main.css',
+        'css/prettyPhoto.css',
+        'css/price-range.css',
+        'css/responsive.css',
     ];
     public $js = [
+        'js/bootstrap.min.js',
+        'js/contact.js',
+        'js/gmaps.js',
+        'js/html5shiv.js',
+        'js/jquery.js',
+        'js/jquery.prettyPhoto.js',
+        'js/jquery.scrollUp.min.js',
+        'js/main.js',
+        'js/price-range.js',
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
