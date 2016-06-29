e200f448b5371c7f820616085236dab7840e6323
wsst17 <jurapapunitsky@gmail.com>
2016-06-29 16:12:33 +0300

    categories widget in process bug in categories brand in view

--- common/widgets/Category.php
+++ common/widgets/Category.php
@@ -2,12 +2,31 @@
 
 namespace common\widgets;
 
+use backend\models\Brand;
 use yii\bootstrap\Widget;
 
 class Category extends Widget
 {
+
+    public function getWidgetData()
+    {
+        $dataWidget = [];
+
+        foreach(\backend\models\Category::find()->all() as $category)
+        {
+            $dataWidget[$category['name']] = [
+                'category' => $category,
+                'brands' => Brand::find()->where(['category_id' => $category['id']])->all(),
+            ];
+        }
+        return $dataWidget;
+    }
+
+
     public function run()
     {
-        return $this->render('/widgets/category');
+        return $this->render('/widgets/category',[
+            'data' => $this->getWidgetData(),
+            ]);
     }
 }
